96c071cb5fe41db118a7ccaf268a6b9ee1c9ee98
VRojas1603 <rojas.vmanuel16@gmail.com>
2025-11-14 08:35:32 -0500

    feat: implement value objects.

---
+++ src/main/java/pe/edu/upc/psychotest_platform/test/domain/model/valueobjects/CategoriesExam.java
@@ -0,0 +1,21 @@
+package pe.edu.upc.psychotest_platform.test.domain.model.valueobjects;
+
+public enum CategoriesExam {
+    PREMIUN(1),
+    NOPREMIUN(2);
+
+    private final int value;
+
+    CategoriesExam(int value) {
+        this.value = value;
+    }
+
+    public static CategoriesExam fromName(String name) {
+        for (CategoriesExam type : values()) {
+            if (type.name().equalsIgnoreCase(name)) {
+                return type;
+            }
+        }
+        throw new IllegalArgumentException("Invalid category type: " + name);
+    }
+}
