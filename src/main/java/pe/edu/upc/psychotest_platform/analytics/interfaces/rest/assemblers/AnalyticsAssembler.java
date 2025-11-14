607cce06ac880869d470e6aa24e67c0b7bfcad52
VRojas1603 <rojas.vmanuel16@gmail.com>
2025-11-14 05:38:47 -0500

    feat: implement assembler.

---
+++ src/main/java/pe/edu/upc/psychotest_platform/analytics/interfaces/rest/assemblers/AnalyticsAssembler.java
@@ -0,0 +1,9 @@
+package pe.edu.upc.psychotest_platform.analytics.interfaces.rest.assemblers;
+
+import pe.edu.upc.psychotest_platform.analytics.domain.model.aggregates.StudentAnalytics;
+import pe.edu.upc.psychotest_platform.analytics.domain.model.aggregates.PsychologistAnalytics;
+
+public class AnalyticsAssembler {
+
+    // Methods to convert to responses if needed
+}
