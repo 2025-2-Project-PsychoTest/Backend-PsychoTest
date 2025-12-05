4bee1035ec1ad2234a86a390699dc4f2694eb8d1
VRojas1603 <rojas.vmanuel16@gmail.com>
2025-12-05 04:45:24 -0500

    feat(profiles): create queries.

---
+++ src/main/java/pe/edu/upc/psychotest_platform/profiles/domain/model/queries/GetAllProfilesQuery.java
@@ -0,0 +1,9 @@
+package pe.edu.upc.psychotest_platform.profiles.domain.model.queries;
+
+/**
+ * GetAllProfilesQuery.
+ * Query to get all profiles.
+ */
+public record GetAllProfilesQuery() {
+}
+
---
+++ src/main/java/pe/edu/upc/psychotest_platform/profiles/domain/model/queries/GetProfileByIdQuery.java
@@ -0,0 +1,9 @@
+package pe.edu.upc.psychotest_platform.profiles.domain.model.queries;
+
+/**
+ * GetProfileByIdQuery.
+ * Query to get a profile by its ID.
+ */
+public record GetProfileByIdQuery(Long profileId) {
+}
+
